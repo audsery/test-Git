0d6755bdc90b46c63dd9625f2bc3b423597b9d2a
eclipse-liujia <eclipse@liujia.com>
2020-03-19 09:21:32 +0800

    my frist eclipse commit

---
+++ src/com/javase/liujia/Login.java
@@ -0,0 +1,13 @@
+package com.javase.liujia;
+
+public class Login {
+
+	public Login() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void m(){
+		System.out.println("12345");
+		
+	}
+
+}
---
+++ src/com/javase/servlet/Intion.java
@@ -0,0 +1,13 @@
+package com.javase.servlet;
+
+public class Intion {
+
+	public Intion() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void m1(){
+		System.out.println("54321");
+		
+	}
+
+}
